2a9c8575140eac1c30aed95985c1d2e536fee029
Ihor Kytsak <kitsak735@gmail.com>
2022-01-14 14:05:30 +0200

    create Data function

--- js/domTask4.js
+++ js/domTask4.js
@@ -4,15 +4,6 @@ let button = document.querySelector('.write-comment__btn');
 
 button.addEventListener('click', addComent);
 
-let date = new Date();
-let curr_date = ('0' + date.getDate()).slice(-2);
-let curr_month = ('0' + (date.getMonth() + 1)).slice(-2);
-let curr_year = date.getFullYear();
-let curr__hour = date.getHours();
-let curr__min = date.getMinutes();
-let now = `${curr_date}.${curr_month}.${curr_year}, 
-${curr__hour}:${curr__min}`
-
 function addComent(event) {
    event.preventDefault();
 
@@ -29,10 +20,23 @@ function addComent(event) {
 
    textItem.innerHTML = `"${textArea.value}"`;
    comentItem.querySelector('h2').innerHTML = 'Unknown User';
-   comentItem.querySelector('.fan-coments__date').innerHTML = now;
+   comentItem.querySelector('.fan-coments__date').innerHTML = getCurrentDate();
 
    comentList.append(comentItem);
 
    textArea.value = '';
    textArea.style.cssText = `box-shadow: none`
+}
+
+function getCurrentDate() {
+   let date = new Date();
+   let curr_date = ('0' + date.getDate()).slice(-2);
+   let curr_month = ('0' + (date.getMonth() + 1)).slice(-2);
+   let curr_year = date.getFullYear();
+   let curr__hour = ('0' + date.getHours()).slice(-2);
+   let curr__min = ('0' + date.getMinutes()).slice(-2);
+   let now = `${curr_date}.${curr_month}.${curr_year}, 
+${curr__hour}:${curr__min}`
+
+   return now;
 }
